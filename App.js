fa7f80719844ab7032c9fa161240e7cb525a67a7
amineaouini-notbot <aminaouini565@gmail.com>
2024-04-28 18:12:24 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, StyleSheet, Text, View } from 'react-native';
+import { windowHeight, windowWidth } from './src/utils/Dimensions';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.wrapper}>
+      <View style={styles.container}>
+        <Animated.View style={styles.box}/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    width: windowWidth - 40,
+    height: windowHeight / 2,
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "orange"
+  }
+
 });
