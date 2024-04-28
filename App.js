755115f39bb7df68bc903a2d06a4137dbdd043b8
amineaouini-notbot <aminaouini565@gmail.com>
2024-04-28 18:42:57 +0100

    squence position loop

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+import { Animated, Easing, StyleSheet, Text, View } from 'react-native';
 import { windowHeight, windowWidth } from './src/utils/Dimensions';
+import { useEffect, useRef } from 'react';
 
 export default function App() {
+  const translateX = useRef(new Animated.Value(0)).current
+  const translateY = useRef(new Animated.Value(0)).current
+  const maxPositionY = windowHeight / 2 - 100
+  const maxPositionX = windowWidth - 140
+  useEffect(()=>{
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(translateY, {
+          useNativeDriver: true,
+          toValue: maxPositionY,
+          duration: 1500
+        }),
+        Animated.timing(translateX, {
+          toValue: maxPositionX,
+          duration: 900,
+          easing: Easing.bounce,
+          useNativeDriver: true
+        }),
+        Animated.timing(translateY, {
+          useNativeDriver: true,
+          toValue: 0,
+          duration: 1500
+        }),
+        Animated.timing(translateX, {
+          toValue: 0,
+          duration: 900,
+          easing: Easing.bounce,
+          useNativeDriver: true
+        })
+  
+      ])
+    ).start()
+  }, [])
+
   return (
     <View style={styles.wrapper}>
       <View style={styles.container}>
-        <Animated.View style={styles.box}/>
+        <Animated.View 
+          style={[styles.box, 
+            {
+              transform: [{translateX}, {translateY}]
+            }
+          ]}
+        />
       </View>
     </View>
   );
